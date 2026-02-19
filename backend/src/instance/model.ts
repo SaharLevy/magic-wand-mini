079df6d2d0235c319fe7ad4ef2b5a528ffa17e55
Sahar Levy <xvruii@gmail.com>
2026-02-20 01:36:29 +0200

    refactor: -Added type safety to questions in model file. -refactored a simple validation to prevent a bug.

--- backend/src/instance/model.ts
+++ backend/src/instance/model.ts
@@ -1,32 +1,61 @@
 import mongoose, { Schema, Types } from "mongoose";
-import { type IInstance, type IAnswer, InstanceStatus } from "./types.js";
+import {
+  type IInstance,
+  type IAnswer,
+  InstanceStatus,
+  IText,
+  IScale,
+  IOption,
+  IOptions,
+  IDate,
+  ITime,
+  IRadioTable,
+  ICheckboxTable,
+} from "./types.js";
 import { QuestionTypes } from "../shared/types.js";
 import config from "../utils/config.js";
 
-const textSchema = new Schema({ text: { type: String, required: true } });
+const textSchema = new Schema<IText>({
+  text: { type: String, required: true },
+});
 
-const scaleSchema = new Schema({
+const scaleSchema = new Schema<IScale>({
   scaleNumber: { type: Number, required: true },
 });
 
-const optionSchema = new Schema({
+const optionSchema = new Schema<IOption>({
   option: { type: String, required: true },
 });
 
-const optionsSchema = new Schema({
+const optionsSchema = new Schema<IOptions>({
   options: { type: [String], default: [] },
 });
 
-const tableAnswerSchema = new Schema({
-  rows: { type: Number, default: [] },
-  columns: { type: [Number], default: [] },
+const radioTableSchema = new Schema<IRadioTable>({
+  tableAnswers: [
+    {
+      row: { type: Number, required: true },
+      column: { type: Number, required: true },
+      _id: false,
+    },
+  ],
 });
 
-const dateSchema = new Schema({
+const checkboxTableSchema = new Schema<ICheckboxTable>({
+  tableAnswers: [
+    {
+      row: { type: Number, required: true },
+      columns: { type: [Number], default: [] },
+      _id: false,
+    },
+  ],
+});
+
+const dateSchema = new Schema<IDate>({
   date: { type: Date },
 });
 
-const timeSchema = new Schema({
+const timeSchema = new Schema<ITime>({
   time: { type: String },
 });
 
@@ -45,8 +74,11 @@ baseAnswerSchema.discriminator(QuestionTypes.LINEAR_SCALE, scaleSchema);
 baseAnswerSchema.discriminator(QuestionTypes.RADIO, optionSchema);
 baseAnswerSchema.discriminator(QuestionTypes.DROPDOWN, optionSchema);
 baseAnswerSchema.discriminator(QuestionTypes.CHECKBOX, optionsSchema);
-baseAnswerSchema.discriminator(QuestionTypes.CHECKBOX_TABLE, tableAnswerSchema);
-baseAnswerSchema.discriminator(QuestionTypes.RADIO_TABLE, tableAnswerSchema);
+baseAnswerSchema.discriminator(
+  QuestionTypes.CHECKBOX_TABLE,
+  checkboxTableSchema,
+);
+baseAnswerSchema.discriminator(QuestionTypes.RADIO_TABLE, radioTableSchema);
 baseAnswerSchema.discriminator(QuestionTypes.DATE, dateSchema);
 baseAnswerSchema.discriminator(QuestionTypes.TIME, timeSchema);
 
