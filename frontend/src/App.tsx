9d6e1ac5dd8d8f59a120a218906a41b3e81f0b3b
Sahar Levy <xvruii@gmail.com>
2026-02-21 22:32:58 +0200

    feat: worked on project set-up

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Magic Wand Form Builder</h1>
+        <p>Hello world.</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
