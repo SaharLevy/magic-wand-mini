74d0514ff02e5a3c8d6e0579363ef9aaca5f51cf
SaharLevy <xvruii@gmail.com>
2026-06-01 15:51:02 +0300

    feat: small change.

---
+++ frontend/src/pages/HomePage/components/FormCard/FormCard.tsx
@@ -0,0 +1,11 @@
+
+interface FormCardProps {
+  formTitle: string;
+  formId: string;
+}
+
+const FormCard = ({ formTitle, formId, formStatus }: FormCardProps) => {
+  return  <div></div> 
+};
+
+export default FormCard;
